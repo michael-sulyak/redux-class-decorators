5bc247f2c644665e5b41c6732663c4f06608519a
expert_m <m.sulyak@gmail.com>
2019-06-11 08:15:40 +0700

    Updates

---
+++ precache-manifest.80c6d264c4655a21f98f531af218b552.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb6d3bcc073724bf509d1a1cae9e378",
+    "url": "/redux-class-decorators/index.html"
+  },
+  {
+    "revision": "152ebfc7055fb9e8744d",
+    "url": "/redux-class-decorators/static/css/main.63c5f3ed.chunk.css"
+  },
+  {
+    "revision": "ca990f0b6c12763f7fbd",
+    "url": "/redux-class-decorators/static/js/2.a519ab94.chunk.js"
+  },
+  {
+    "revision": "152ebfc7055fb9e8744d",
+    "url": "/redux-class-decorators/static/js/main.465b7afa.chunk.js"
+  },
+  {
+    "revision": "c460b06ccfec994b9b7c",
+    "url": "/redux-class-decorators/static/js/runtime~main.5fdac967.js"
+  }
+]);
